fc4b79ce617f1ed6e23655a16d2d745d683a18ad
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2021-03-31 15:12:18 +0200

    :boom: functioning managa_links.php

--- manage_links.php
+++ manage_links.php
@@ -1,5 +1,22 @@
-<?php require_once("./tpl/header.php"); ?>
+<?php 
+    require_once "./tpl/header.php"; 
+    require_once "connection_db.php";
+    $redirect_link = "localhost:8888/puce-app/";
 
+    try {
+        $req = $bdd->prepare("SELECT url, code, clicks, creation_date FROM links");
+        $res = $req->execute();
+        $res = $req->fetchAll();
+        $links_number = count($res);
+    } catch (Exception $e) {
+        die("Error: ".$e);
+    }
+
+    function get_date($str) {
+        $to_date = strtotime($str);
+        return date("d-m-Y",$to_date);
+    }
+?>
 <div>
     <h1 class="title-text">Gérer mes liens</h1>
 </div>
@@ -7,7 +24,7 @@
 <div class="container">
     <div class="category-titles">
         <p>
-            9 lien(s)
+            <?= $links_number." lien.s" ?>
         </p>
         <p>
             clic(s)
@@ -16,23 +33,34 @@
             actions
         </p>
     </div>
-    <hr></hr>
-    <div>
-        <div class="link-made">
-            <div>
-                <p class="date">17 mars 2020</p>
-                <p class="long-link">www.unsupersitemaislurlestbcptroplong.com</p>
-                <p class="short-link">puce/37mmnwht</p>
-            </div>
-            <div class="number-clicks">5</div>
-            <div class="actions">
-                <img src="./img/garbage.png" class="garbage" alt="garbage can">
-                <div class="copy-button">
-                    <a href="#" class="copy-link">copier</a>
-                    <!-- <button class="copy-link">copier</button> -->
+    <div class="link-elem">
+        <?php 
+            foreach ($res as $row) {
+                echo 
+                '
+                    <div class= "link">
+                    <hr></hr>
+                    <div>
+                        <div class="link-made">
+                            <div>
+                                <p class="date">'.get_date($row['creation_date']).'</p>
+                                <p class="long-link">'.$row['url'].'</p>
+                                <a href="http://'.$redirect_link.$row['code'].'"class="short-link">'.$redirect_link.$row['code'].'</p>
+                            </div>
+                            <div class="number-clicks">'.$row['clicks'].'</div>
+                            <div class="actions">
+                                <img src="./img/garbage.png" class="garbage" alt="garbage can">
+                                <div class="copy-button">
+                                    <a href="#" class="copy-link">copier</a>
+                                    <button class="copy-link">copier</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+                ';
+            }
+        ?>
         <hr></hr>
     </div>
 </div>
