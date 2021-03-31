9c9937afb29c9d2166386c7e74d81d97da6ba916
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2021-03-31 22:11:08 +0200

    :sparkles: add 404 error handler

---
+++ error_doc.php
@@ -0,0 +1,9 @@
+<?php 
+require_once "./tpl/header.php" 
+?>
+
+
+<div class="error-404-container">
+    <img class="error-404" src="./img/error404.svg" alt="Error 404">
+    <p>Woops something went wrong...</p>
+</div>
